b1406a72cc7bd1e1ac8b9de2ffdd3f06c4ea53d5
Ritwik Tiwari <ritwiktiwari24@gmail.com>
2020-05-22 13:12:19 +0530

    jobs app setup complete

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -77,8 +77,11 @@ WSGI_APPLICATION = 'portfolio.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'portfolio',
+        'USER': 'postgres',
+        'PASSWORD': 'hello123',
+        'HOST': 'localhost',
     }
 }
 
@@ -120,3 +123,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
