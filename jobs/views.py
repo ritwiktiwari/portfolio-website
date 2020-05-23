a3f8810f72b478d5bb2eb241d097746d856f3c29
Ritwik Tiwari <ritwiktiwari24@gmail.com>
2020-05-24 00:23:42 +0530

    added pagination

--- jobs/views.py
+++ jobs/views.py
@@ -1,11 +1,15 @@
 from django.shortcuts import render
 from .models import Job
+from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 # Create your views here.
 
 
 def index(request):
-    jobs = Job.objects.all()
+    jobs = Job.objects.order_by('-list_date').filter(is_published=True)
+    paginator = Paginator(jobs, 6)
+    page = request.GET.get('page')
+    paged_jobs = paginator.get_page(page)
     context = {
-        'jobs': jobs,
+        'jobs': paged_jobs,
     }
     return render(request, 'jobs/index.html', context)
