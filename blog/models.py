51304160c8de86dec1ab088175ad3ed58a69c2ce
Ritwik Tiwari <ritwiktiwari24@gmail.com>
2020-05-23 18:34:29 +0530

    template and url setup for jobs app

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from datetime import datetime
 
-# Create your models here.
+
+class Blog(models.Model):
+    title = models.CharField(max_length=200)
+    pub_date = models.DateTimeField(default=datetime.now(), blank=True)
+    body = models.TextField()
+    image = models.ImageField(upload_to='images/')
