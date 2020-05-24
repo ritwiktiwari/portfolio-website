c45de22e362a30d8383c2a991d19a13e4a2b6451
Ritwik Tiwari <ritwiktiwari24@gmail.com>
2020-05-25 00:14:45 +0530

    homepage complete

--- jobs/views.py
+++ jobs/views.py
@@ -1,15 +1,18 @@
 from django.shortcuts import render
 from .models import Job
+from photos.models import Photo
 from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 # Create your views here.
 
 
 def index(request):
+    photos = Photo.objects.order_by('list_date').filter(is_published=True)
     jobs = Job.objects.order_by('-list_date').filter(is_published=True)
     paginator = Paginator(jobs, 6)
     page = request.GET.get('page')
     paged_jobs = paginator.get_page(page)
     context = {
         'jobs': paged_jobs,
+        'photos': photos,
     }
     return render(request, 'jobs/index.html', context)
--- portfolio/settings.py
+++ portfolio/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'blog.apps.BlogConfig',
     'jobs.apps.JobsConfig',
+    'photos.apps.PhotosConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
